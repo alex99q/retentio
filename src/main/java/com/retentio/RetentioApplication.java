e3a715611a27ed67e16476d869cc5ed52a1b9fad
Alexander Kanev <alex99q@abv.bg>
2020-12-18 17:25:06 +0200

    Initial setup of Spring

---
+++ src/main/java/com/retentio/RetentioApplication.java
@@ -0,0 +1,13 @@
+package com.retentio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetentioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetentioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retentio/RetentioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retentio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetentioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
