4179f7f8d524a9ddb2c023f3aa4273665df6d449
Alexander Kanev <alex99q@abv.bg>
2021-01-18 18:53:35 +0200

    Added a navbar fragment

---
+++ src/main/java/com/retentio/controller/ProfileController.java
@@ -0,0 +1,17 @@
+package com.retentio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ProfileController {
+
+    @RequestMapping(value = "/profile", method = RequestMethod.GET)
+    public ModelAndView profile() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/profile");
+        return modelAndView;
+    }
+}
